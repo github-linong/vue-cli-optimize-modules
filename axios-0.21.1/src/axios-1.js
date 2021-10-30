78f5ca00d50aef11b3a57758cfd43ede06b8d143
linong <linong@kuaishou.com>
2021-10-30 16:01:40 +0800

    fix: 测试部分包，删除node_modules发现可以使用相同的包

--- axios-0.21.1/src/axios-1.js
+++ axios-0.21.1/src/axios-1.js
@@ -2,5 +2,5 @@
 import axios from 'axios'
 
 axios('https://www.lilnong.top/cors/axios-0.21.1/axios-1')
-console.log('axios@0.21.1', 'axios-1', '调用')
+console.log('axios@0.21.1', 'axios-1', '调用', axios.VERSION)
 export default axios;
--- axios-no/src/axios-1.js
+++ axios-no/src/axios-1.js
@@ -2,5 +2,5 @@
 import axios from 'axios'
 
 axios('https://www.lilnong.top/cors/axios-no/axios-1')
-console.log('axios@no', 'axios-1', '调用')
+console.log('axios@no', 'axios-1', '调用', axios.VERSION)
 export default axios;
